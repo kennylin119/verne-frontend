588af660bf82f06e175936d67ca06f3f818817e7
Kenny Lin <kennylin119@gmail.com>
2025-06-20 13:02:25 -0400

    added basic support for users entering in questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import './App.css'
 
 function App() {
   const [showInputBar, setShowInputBar] = useState(false);
+  const [inputText, setInputText] = useState('');
+  const [questions, setQuestions] = useState([]);
+  // const [answers, setAnswers] = useState([]);
+
+  // when user presses enter, if inputText is not empty, add it to questions array and clear inputText
+  const handleEnter = (e) => {
+    if (e.key === 'Enter' && inputText.trim() !== '') {
+      setQuestions((prev) => [...prev, inputText.trim()]);
+      setInputText('');
+      setShowInputBar(false);
+    }
+  }
 
   return (
     <div className="h-screen w-full flex flex-col bg-gradient-to-b from-[#694A4A] to-[#221C1C] text-[#FFF4D7] text-lg font-Caudex px-1 py-2"> 
@@ -25,6 +37,16 @@ function App() {
           velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora
           torquent per conubia nostra, per inceptos himenaeos.
         </p>
+        
+        <div className="space-y-4 mb-6">
+          {questions.map((question, index) => (
+            <div key={index} className="flex items-center gap-2 mb-10 w-full justify-end text-[#FFFFFF]">
+              <Pencil size={22} strokeWidth={2} className="p-1 border border-current rounded-full"/>
+              {question}
+            </div>
+            ))}
+        </div>
+
         {/* mute + save icons */}
         <div className="flex items-center gap-2 mb-4">
           <button className="p-1 border border-current rounded-full">
@@ -52,7 +74,10 @@ function App() {
         <div className="flex items-center gap-3">
           <input
             type="text"
-            placeholder="What does th"
+            placeholder="What's your question?"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            onKeyDown={handleEnter}
             className="flex-1 p-2 bg-[#322B26] border border-[#F5E8C7]/90 rounded-lg text-white focus:outline-none focus:ring-2 focus:ring-[#F5E8C7]/90"
           />
           <button 
